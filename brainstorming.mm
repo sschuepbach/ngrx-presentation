edf7432864eeab398e622fe5cf91060986ea847e
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-04-03 16:31:08 +0200

    First brainstorming session

---
+++ brainstorming.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Pr&#xe4;sentation NgRx" FOLDED="false" ID="ID_296968568" CREATED="1522765389728" MODIFIED="1522765400959" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Was ist State?" POSITION="right" ID="ID_702461525" CREATED="1522765404479" MODIFIED="1522765411286">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Elemente von NgRx" POSITION="right" ID="ID_949443333" CREATED="1522765418088" MODIFIED="1522765426617">
+<edge COLOR="#0000ff"/>
+<node TEXT="Reducers" ID="ID_1641421349" CREATED="1522765430739" MODIFIED="1522765434642"/>
+<node TEXT="Actions" ID="ID_746542189" CREATED="1522765436636" MODIFIED="1522765438010"/>
+<node TEXT="Selectors" ID="ID_575460382" CREATED="1522765438692" MODIFIED="1522765441089"/>
+<node TEXT="Effects" ID="ID_507663549" CREATED="1522765441428" MODIFIED="1522765443953"/>
+<node TEXT="Store" ID="ID_1351789804" CREATED="1522765799577" MODIFIED="1522765802145"/>
+</node>
+<node TEXT="Ressourcen" POSITION="left" ID="ID_1293765201" CREATED="1522765447244" MODIFIED="1522765449523">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Vorteile von NgRx" POSITION="left" ID="ID_7076711" CREATED="1522765457443" MODIFIED="1522765463492">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Nachteile von NgRx" POSITION="right" ID="ID_760664671" CREATED="1522765466087" MODIFIED="1522765470209">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Extensions" POSITION="right" ID="ID_780995686" CREATED="1522765473712" MODIFIED="1522765476325">
+<edge COLOR="#7c0000"/>
+<node TEXT="Entity" ID="ID_1846702102" CREATED="1522765478519" MODIFIED="1522765486544"/>
+<node TEXT="Router-store" ID="ID_1706849664" CREATED="1522765487152" MODIFIED="1522765553364"/>
+<node TEXT="Schematics" ID="ID_290268802" CREATED="1522765488992" MODIFIED="1522765496701"/>
+<node TEXT="Store-devtools" ID="ID_300217234" CREATED="1522765497128" MODIFIED="1522765560586"/>
+<node TEXT="(Effects)" ID="ID_1010018626" CREATED="1522765566527" MODIFIED="1522765569678"/>
+</node>
+<node TEXT="Aufbau NgRx-Applikation" POSITION="left" ID="ID_804313597" CREATED="1522765577960" MODIFIED="1522765584488">
+<edge COLOR="#00007c"/>
+<node TEXT="Konfiguration Module" ID="ID_497141956" CREATED="1522765587199" MODIFIED="1522765594572">
+<node TEXT="root-Modul" ID="ID_311893609" CREATED="1522765596480" MODIFIED="1522765601299"/>
+<node TEXT="feature-Module" ID="ID_1906882601" CREATED="1522765602436" MODIFIED="1522765607416"/>
+<node TEXT="Weitere Verzeichnisse (actions, effects, reducers)" ID="ID_46615708" CREATED="1522765611699" MODIFIED="1522765650410"/>
+<node TEXT="index.ts" ID="ID_1685382344" CREATED="1522765630155" MODIFIED="1522765633256"/>
+</node>
+</node>
+<node TEXT="NgRx in a Nutshell" POSITION="left" ID="ID_773454224" CREATED="1522765677512" MODIFIED="1522765681981">
+<edge COLOR="#007c00"/>
+<node TEXT="Immutable state" ID="ID_573866557" CREATED="1522765684624" MODIFIED="1522765772381"/>
+<node TEXT="Zentrale Verwaltung von State" ID="ID_1783158215" CREATED="1522765776056" MODIFIED="1522765790688"/>
+</node>
+</node>
+</map>
